6312bc4ce3160d36f58de0705cd52f903e7232cc
bhasker9429 <bhasker9429@gmail.com>
2026-05-25 11:30:41 -0400

    Create Wildfly_version.py

---
+++ Wildfly_version.py
@@ -0,0 +1,13 @@
+# Define your hardcoded WildFly version info
+wildfly_versions = {
+    "dev": "34.0.0.Final",
+}
+
+# Select the target environment
+env = "dev"
+
+# Access the version
+if env in wildfly_versions:
+    print(f"Target WildFly version for {env} is {wildfly_versions[env]}")
+else:
+    print("Environment not found.")
